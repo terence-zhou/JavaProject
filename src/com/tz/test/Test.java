db1e6d0fe40d69992cd570c5c4826cd73576e5ba
terence-zhou <magicmars@sina.com>
2017-08-29 17:25:09 +0800

    产生冲突了

--- src/com/tz/test/Test.java
+++ src/com/tz/test/Test.java
@@ -5,7 +5,10 @@ public class Test
     
     public static void main(String[] args)
     {
+        System.out.println("==========================");
         System.out.println("==========又一个测试项目===========");
+        System.out.println("==========================");
+        System.out.println(System.currentTimeMillis());
     }
     
 }
