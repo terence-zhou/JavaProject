90f9bfea6929552e34566911d550f7913a7f83e6
车站男生 <magicmars@sina.com>
2017-08-29 17:22:30 +0800

    尝试修改产生冲突
    加了点代码

--- src/com/tz/test/Test.java
+++ src/com/tz/test/Test.java
@@ -5,7 +5,9 @@ public class Test
     
     public static void main(String[] args)
     {
+        System.out.println("==========================");
         System.out.println("==========又一个测试项目===========");
+        System.out.println("==========================");
     }
     
 }
