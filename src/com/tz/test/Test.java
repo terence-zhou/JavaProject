905cfcbed94d2c77ac47e9204df058a440c619e3
terence-zhou <magicmars@sina.com>
2017-09-04 17:39:17 +0800

    DSF

--- src/com/tz/test/Test.java
+++ src/com/tz/test/Test.java
@@ -6,7 +6,7 @@ public class Test
     public static void main(String[] args)
     {
         //我是注释
-        //继续加内容
+        //继续加内容XXXX
         System.out.println("==========================");
         System.out.println("==========又一个测试项目===========");
         System.out.println("==========================");
